06a7d245a628082d8a6949de1ec51dad250b29ee
NPJingZhan <s10266800@connect.np.edu.sg>
2024-11-27 22:52:19 +0800

    Added top biggest tournaments hosted

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ let lastScrollY = window.scrollY;
 
   window.addEventListener('scroll', () => {
     if (window.scrollY > lastScrollY) {
-      // Scrolling down
       navbar.style.transform = 'translateY(-100%)'; // Hide navbar
     } else {
-      // Scrolling up
       navbar.style.transform = 'translateY(0)'; // Show navbar
     }
     lastScrollY = window.scrollY;
   });
+
+  document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        const offset = 200; // Adjust based on your header height
+        const targetPosition = target.offsetTop - offset;
+
+        window.scrollTo({
+            top: targetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
